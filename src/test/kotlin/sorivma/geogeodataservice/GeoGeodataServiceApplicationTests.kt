21ea80ea4fdeccdeb7d05c693562a04a2029d198
sorivma <sorivma03.06@gmail.com>
2025-04-04 00:38:20 +0300

    Initial commit

---
+++ src/test/kotlin/sorivma/geogeodataservice/GeoGeodataServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package sorivma.geogeodataservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GeoGeodataServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
